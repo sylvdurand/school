371b838d0fd6f9bef960e1222a32204c701066e8
sylvdurand <sylv.durand@gmail.com>
2016-10-30 07:37:11 +0100

    add more classes

--- school/models.py
+++ school/models.py
@@ -8,7 +8,9 @@ TYPE_CLASSE = (
     ('MS', 'Moyenne section'),
     ('GS', 'Grande section'),
     ('CP', 'CP'),
-    ('CE1', 'CE1'),
+    ('CE1A', 'CE1 A'),
+    ('CE1B', 'CE1 B'),
+    ('CE1C', 'CE1 C'),
     ('CE2', 'CE2'),
     ('CM1', 'CM1'),
     ('CM2', 'CM2'),
@@ -24,7 +26,7 @@ class Dictee(models.Model):
     text = models.TextField(default='')
     type = models.CharField(max_length=1, choices=TYPE_DE_DICTEE, default='T')
     created_date = models.DateTimeField(default=timezone.now)
-    classe = models.CharField(max_length=3,choices=TYPE_CLASSE, default='CE1', null=True)
+    classe = models.CharField(max_length=6,choices=TYPE_CLASSE, default='CE1C', null=True)
     niveau = models.PositiveSmallIntegerField(null=True, default=0)
     def publish(self):
         self.published_date = timezone.now()
@@ -36,7 +38,7 @@ class Dictee(models.Model):
 class Enfant(models.Model):
     prenom = models.CharField(max_length=20)
     nom = models.CharField(max_length=30)
-    classe = models.CharField(max_length=3,choices=TYPE_CLASSE, default='CE1', null=True)
+    classe = models.CharField(max_length=6,choices=TYPE_CLASSE, default='CE1C', null=True)
     def __str__(self):
         return '%s %s' % (self.prenom, self.nom)
 
@@ -47,7 +49,7 @@ class Probleme(models.Model):
     formule_resultat = models.CharField(default='',max_length=200)
     unite_resultat = models.CharField(default='',max_length=20)
     created_date = models.DateTimeField(default=timezone.now)
-    classe = models.CharField(max_length=3,choices=TYPE_CLASSE, default='CE1', null=True)
+    classe = models.CharField(max_length=6,choices=TYPE_CLASSE, default='CE1C', null=True)
 
     def __str__(self):
         return self.title
